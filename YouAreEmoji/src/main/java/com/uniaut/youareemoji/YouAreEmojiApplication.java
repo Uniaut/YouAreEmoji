d1daa43c1b74beda8bd0bce58916ca63d169c3e3
Uniaut <kunwoo0927@gmail.com>
2024-03-05 22:48:09 +0900

    [chore] init project

---
+++ YouAreEmoji/src/main/java/com/uniaut/youareemoji/YouAreEmojiApplication.java
@@ -0,0 +1,13 @@
+package com.uniaut.youareemoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAreEmojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAreEmojiApplication.class, args);
+    }
+
+}
---
+++ YouAreEmoji/src/test/java/com/uniaut/youareemoji/YouAreEmojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniaut.youareemoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouAreEmojiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
